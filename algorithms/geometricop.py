04a2113a02fc6701abf845b5610226b24284e07e
Tobi <tobi.seyde@gmail.com>
2018-03-08 21:21:37 +0100

    Centroid

--- algorithms/geometricop.py
+++ algorithms/geometricop.py
@@ -12,31 +12,47 @@ def polygon_area(points):
     if len(points) < 2 or points[0] != points[-1]:
         raise ValueError
 
+    n = len(points)-1
+
     area = 0
 
-    for i, point in enumerate(points):
-        x_i, y_i = point
-        x_ii, y_ii = points[i + 1]
+    for i in range(n):
+        x_i, y_i = points[i]
+        x_ii, y_ii = points[i+1]
 
         area += x_i*y_ii - x_ii*y_i
 
-        if i == len(points)-2:
-            break
-
-    return abs(0.5 * area)
+    return 0.5 * area
 
 
 def polygon_centroid(points):
-    pass
+    n = len(points)-1
+
+    c_x = 0
+    c_y = 0
+
+    area = polygon_area(points)
+
+    for i in range(n):
+        x_i, y_i = points[i]
+        x_ii, y_ii = points[i+1]
+
+        c_x += (x_i + x_ii) * (x_i*y_ii - x_ii*y_i)
+        c_y += (y_i + y_ii) * (x_i*y_ii - x_ii*y_i)
+
+    c_x = 1/(6*area) * c_x
+    c_y = 1/(6*area) * c_y
+
+    return c_x, c_y
 
 
 if __name__ == '__main__':
     ccw = [
         (1, 1),
-        (1, 3),
-        (3, 3),
         (3, 1),
+        (3, 3),
+        (1, 3),
         (1, 1),
     ]
     print(polygon_area(ccw))
-
+    print(polygon_centroid(ccw))
